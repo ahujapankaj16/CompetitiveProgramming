5ef5d76544caecd481b1753f63d0593a60b8df3f
ahujapankaj16 <32018631+ahujapankaj16@users.noreply.github.com>
2019-11-10 19:18:22 +0530

    Update ReverseLinkedLIstmn.py

--- ReverseLinkedLIstmn.py
+++ ReverseLinkedLIstmn.py
@@ -1,10 +1,11 @@
-# Definition for singly-linked list.
+
 
 '''
 Reverse a linked list from position m to n. Do it in one-pass.
 Input: 1->2->3->4->5->NULL, m = 2, n = 4
 Output: 1->4->3->2->5->NULL
 '''
+# Definition for singly-linked list.
 class ListNode:
     def __init__(self, x):
         self.val = x
